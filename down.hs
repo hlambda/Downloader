3cbcdd40ae2bd2c343f63b756484d84103cabd1c
hlambda <webmail.hari@gmail.com>
2015-10-11 15:35:18 +0530

    Some cleanup and updated readme

--- down.hs
+++ down.hs
@@ -2,13 +2,8 @@
 import System.IO
 import System.Environment
 import System.Process
+import Control.Monad
 import qualified Data.ByteString as B
-
--- Problems
--- -------
---
--- The files that are to be combined are binary files.
--- Might require the use of Bytestring package or something similar.
 
 splitSize = 128 * 1024 * 1024 -- 128 Mega Bytes
 
@@ -20,8 +15,6 @@ download url = do
   putStrLn $ show fileSize
   let splits = ((fromIntegral fileSize) / (fromIntegral splitSize))
       rs = ceiling splits
-      -- Room for parallel download'?
-      -- Note that there is a parallel map function
       xss = map (\x -> download' url x fileSize)  [1..rs]
   putStrLn $ "Number of splits: " ++ (show rs)
   foldr (>>) (return ()) xss
@@ -29,9 +22,8 @@ download url = do
 
 download' :: String -> Int -> Int -> IO ()
 download' url n tot = do
-  -- will continue work properly?
-  -- Corner case? What if we have reached the end?
-  system $ "curl -o \"file." ++ (show n) ++ "\" --range " ++ start ++  "-" ++ end ++ " " ++ url
+  system $ "curl -o \"file." ++ (show n) ++
+             "\" --range " ++ start ++  "-" ++ end ++ " " ++ url
   return ()
     where
       start = show $ (n - 1) * splitSize
@@ -44,14 +36,11 @@ combine n = do
   all <- foldr (aux') (return "") xs
   B.writeFile "file" all  
       where aux x = B.readFile $ "file." ++ (show x)
-            aux' s1 s2 = do
-              s1' <- s1
-              s2' <- s2
-              let s3 = s1' `B.append` s2'
-              return s3  
+            aux' :: IO B.ByteString -> IO B.ByteString -> IO B.ByteString
+            aux' s1 s2 = liftM2 B.append s1 s2
 
 main = do
   ar <- fmap head $ getArgs
   putStrLn $ ar
   download ar
-  return ()
+
