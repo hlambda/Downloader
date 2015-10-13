cc2f1947d7364417297c342f996b828243831f33
hlambda <webmail.hari@gmail.com>
2015-10-13 16:54:18 +0530

    Curl now downloads silently; split size reduced to 20 Megabytes

--- down.hs
+++ down.hs
@@ -6,14 +6,14 @@ import Control.Monad
 import Control.Concurrent.Async
 import qualified Data.ByteString as B
 
-splitSize = 128 * 1024 * 1024 -- 128 Mega Bytes
+splitSize = 20 * 1024 * 1024 -- 128 Mega Bytes
 
 download :: String -> String -> IO ()
 download url fname = do
   system $ "curl -sI " ++ url ++
              " | awk '/Content-Length/ { print $2 }' > /tmp/hacurl"
   fileSize <- fmap (read) $ readFile $ "/tmp/hacurl" :: IO Int
-  putStrLn $ show fileSize
+  putStrLn $ "Filesize: " ++ show fileSize
   let splits = ((fromIntegral fileSize) / (fromIntegral splitSize))
       rs = ceiling splits
   putStrLn $ "Number of splits: " ++ (show rs)
@@ -22,7 +22,7 @@ download url fname = do
 
 download' :: String -> Int -> Int -> String -> IO ()
 download' url n tot fname = do
-  system $ "curl -o \"" ++ fname ++ "." ++ (show n) ++
+  system $ "curl -s -o \"" ++ fname ++ "." ++ (show n) ++
              "\" --range " ++ start ++  "-" ++ end ++ " " ++ url
   return ()
     where
